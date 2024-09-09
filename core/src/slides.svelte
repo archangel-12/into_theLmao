a49300c5ca0c1ed0682c92dfe9d25f7e63f1353b
the nothing <mydeadass1568@gmail.com>
2024-09-09 16:08:17 +0000

    done

--- core/src/slides.svelte
+++ core/src/slides.svelte
@@ -1,61 +1,60 @@
 <script lang="ts">
 	import { Presentation } from '@components';
-	import { startLoop } from './pipe/rememBer';
-
-	import { onMount } from 'svelte';
-	onMount(() => {
-		startLoop();
-	});
 </script>
 
 <Presentation>
 	<div class="reveal">
 		<div class="slides">
 			<section>
-				<section data-background-image="https://media1.tenor.com/m/fQAv9LMYVOIAAAAd/caseoh-caseoh-games.gif">
-					<h1 style="font-size: 3rem; text-transform: none; color: white;">
-						Perpaduan cara berpikir Diakronik dan Sinkronik dalam Sejarah
-					</h1>
+				<section data-background-image="https://media1.tenor.com/m/R9VGT3L_jKAAAAAC/pink-guy-grin.gif">
+					<h1 style="font-size: 4rem">Pergerakan Nasional Indonesia</h1>
 				</section>
-				<section data-background-image="https://media1.tenor.com/m/yKNWg88Y1W4AAAAC/eric-andre-let-me.gif" />
-				<section data-background-iframe="https://en.wikipedia.org/wiki/30_September_Movement" data-background-interactive />
-				<section>
-					<h1 style="font-size: 3rem; text-transform: none; color: white;">
-						Diakronik mementingkan terjadinya suatu peristiwa sedangkan sinkronik mementingkan struktur dalam peristiwa
-					</h1>
+				<section data-background-image="https://media1.tenor.com/m/qEkwUjLJJb8AAAAC/compass.gif">
+					<h1 style="font-size: 4rem">Kebangkitan Nasionalisme di Asia</h1>
+				</section>
+				<section data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)" data-background-image="https://media1.tenor.com/m/1WoHtsUrwFcAAAAd/breaking-bad-handshake.gif">
+					<h2 style="font-size: 4rem">Faktor-faktor yang memicu adanya rasa Nasionalisme di kawasan Asia / Afrika</h2>
+					<div class="r-hstack justify-center">
+						<div data-id="box1" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box2" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box3" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box4" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+					</div>
+				</section>
+				<section data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)" data-background-image="https://media1.tenor.com/m/1WoHtsUrwFcAAAAd/breaking-bad-handshake.gif">
+					<div class="r-hstack justify-center">
+						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 500px; height: 300px; margin: 10px;">Adanya kesamaan nasib akibat imperialisme & kolonialisme</div>
+						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 500px; height: 200px; margin: 10px;">Kenangan akan kejayaan masa lalu</div>
+						<div data-id="box3" data-auto-animate-delay="0.2" style="background: brown; width: 500px; height: 200px; margin: 10px;">Kemunculan kaum terpelajar</div>
+						<div data-id="box4" data-auto-animate-delay="0.3" style="background: black; width: 600px; height: 400px; margin: 10px;">Kemajuan dalam bidang politik, sosial ekonomi, & kebudayaan</div>
+					</div>
+					<h2 style="margin-top: 20px;">Faktor Dalam Negeri</h2>
+				</section>
+				<section data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)" data-background-image="https://media1.tenor.com/m/ezrhrT61gJUAAAAC/lol-blowfish.gif">
+					<div class="r-stack">
+						<div data-id="box1" style="background: cyan; width: 300px; height: 300px;"></div>
+						<div data-id="box2" style="background: magenta; width: 200px; height: 200px;"></div>
+					</div>
+					<h2 style="margin-top: 20px;">Faktor Luar Negeri</h2>
+				</section>
+				<section data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)" data-background-image="https://media1.tenor.com/m/ezrhrT61gJUAAAAC/lol-blowfish.gif">
+					<div class="r-hstack justify-center">
+						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 500px; height: 100px; margin: 10px;">Perang Russo-Jepang</div>
+						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 500px; height: 200px; margin: 10px;">Gerakan Nasionalisme di berbagai wilayah di penjuru Asia</div>
+					</div>
 				</section>
 			</section>
-		</div>
-
-		<div class="slides">
-			<section>
-				<section data-background-image="https://media1.tenor.com/m/I2t5X5BT9CoAAAAd/caseoh-case-oh.gif">
-					<h1 style="font-size: 3rem; text-transform: none; color: white;">
-              			Hubungan sebab akibat (kausalitas), perubahan, & keberlanjutan
-            		</h1>
-				</section>
-				<section data-background-image="https://www.vice.com/wp-content/uploads/sites/2/2017/09/1506753584399-WhatsApp-Image-2017-09-29-at-42942-PM.jpeg?resize=1024,564" />
-				<section data-background-image="https://www.vice.com/wp-content/uploads/sites/2/2017/09/1506753584399-WhatsApp-Image-2017-09-29-at-42942-PM.jpeg?resize=1024,564">
-					<h2 style="font-size: 3rem; text-transform: none; color: white;">
-					  ada yang tahu mereka-mereka ini siapa? :(
-					</h2>
-				</section>
+			<section data-background-image="https://ssl1.sephorastatic.cn/prodpicture/static.gif">
+				<p style="font-size: 2rem;">To all SMANTAS colleague, think about it...</p>
+				<p style="font-size: 2rem;">From the late Mohammad Hatta to a disgusting perverted misogynistic cunt</p>
+				<p style="font-size: 2rem">What could be possibly wrong with this nation?</p>
+				<p style="font-size: larger;">BRADFORD'S ALUMNI YET HIS FUCKING GPA 2.3</p>
+				<q style="font-size: larger;">Representative of the Future Brilliant Youth</q>,
+				<q style="font-size: larger;">Prodigal son worthy of leading 300 million souls</q>,
+				<q style="font-size: larger;">The Surakarta's Messiah</q>,
+				<h1 style="font-size: 4rem;">Alangkah baiknya jika spesies satu ini bunuh diri saja</h1>
+				<h2 style="font-size: 2rem">Apa? lapor ke kapolda jatim perihal pasal penghinaan wakil presiden? GO FOR IT. I couldn't care less. I'd rather rot in cell than having seeing his face every day</h2>
 			</section>
-		</div>
-
-		<div class="slides">
-			<section>
-				<section data-background-image="https://media1.tenor.com/m/Hbfi8voUJ4EAAAAC/bh187-spongebob.gif">
-					<h1 style="font-size: 3rem; text-transform: none; color: black;">
-						Manfaat belajar sejarah
-					</h1>
-				</section>
-				<section data-background-image="https://studio.mrngroup.co/storage/app/media/Prambors/Editorial%204/ramai-peringatan-darurat-logo-garuda-pancasila-dengan-latar-biru-di-sosial-media-ada-apa-20240821172811.webp?tr=w-800" />
-			</section>
-		</div>
-
-		<div class="slides">
-			<section id="supers" />
 		</div>
 	</div>
 </Presentation>
