fbb3cccd3012d8db9f47956cf3be29111fcf5654
the nothing <mydeadass1568@gmail.com>
2024-09-11 16:36:31 +0000

    done

--- core/src/slides.svelte
+++ core/src/slides.svelte
@@ -6,54 +6,61 @@
 	<div class="reveal">
 		<div class="slides">
 			<section>
-				<section data-background-image="https://media1.tenor.com/m/R9VGT3L_jKAAAAAC/pink-guy-grin.gif">
-					<h1 style="font-size: 4rem">Pergerakan Nasional Indonesia</h1>
+				<section data-background-image="https://media.tenor.com/2KdPP9oZqnwAAAAM/django-unchained-whipping.gif">
+					<h1 style="font-size: 4rem">Dampak Penjajahan di Hindia-Belanda</h1>
 				</section>
 				<section data-background-image="https://media1.tenor.com/m/qEkwUjLJJb8AAAAC/compass.gif">
-					<h1 style="font-size: 4rem">Kebangkitan Nasionalisme di Asia</h1>
+					<h1 style="font-size: 4rem">Ekonomi</h1>
 				</section>
-				<section data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)" data-background-image="https://media1.tenor.com/m/1WoHtsUrwFcAAAAd/breaking-bad-handshake.gif">
-					<h2 style="font-size: 4rem">Faktor-faktor yang memicu adanya rasa Nasionalisme di kawasan Asia / Afrika</h2>
-					<div class="r-hstack justify-center">
-						<div data-id="box1" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-						<div data-id="box2" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-						<div data-id="box3" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-						<div data-id="box4" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
-					</div>
+				<section data-background-image="https://assets.lastdodo.com/image/ld_large/plain/assets/catalog/assets/1/4/f/6/pdf_4f6f9180-fecd-012c-a2cb-0050569428b1.jpg" />
+				<section data-background-image="https://assets.lastdodo.com/image/ld_large/plain/assets/catalog/assets/1/4/f/6/pdf_4f6f9180-fecd-012c-a2cb-0050569428b1.jpg">
+					<h1 style="font-size: 4rem; color:black">Uang kertas</h1>
 				</section>
-				<section data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)" data-background-image="https://media1.tenor.com/m/1WoHtsUrwFcAAAAd/breaking-bad-handshake.gif">
-					<div class="r-hstack justify-center">
-						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 500px; height: 300px; margin: 10px;">Adanya kesamaan nasib akibat imperialisme & kolonialisme</div>
-						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 500px; height: 200px; margin: 10px;">Kenangan akan kejayaan masa lalu</div>
-						<div data-id="box3" data-auto-animate-delay="0.2" style="background: brown; width: 500px; height: 200px; margin: 10px;">Kemunculan kaum terpelajar</div>
-						<div data-id="box4" data-auto-animate-delay="0.3" style="background: black; width: 600px; height: 400px; margin: 10px;">Kemajuan dalam bidang politik, sosial ekonomi, & kebudayaan</div>
-					</div>
-					<h2 style="margin-top: 20px;">Faktor Dalam Negeri</h2>
+				<section data-background-image={'./img/ahaha.jpg'} />
+				<section data-background-image="https://www.researchgate.net/publication/381290731/figure/fig1/AS:11431281250648852@1717957759280/Gambar-2-Peta-Kota-Surabaya-berdasarkan-Permukiman-Penduduk-Kulit-Putih-Eropa-blok.png">
+					<p style="font-size: 3rem; color: black">Kaart van de Hoofdstad Soerabaja en Omstreken</p>
 				</section>
-				<section data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)" data-background-image="https://media1.tenor.com/m/ezrhrT61gJUAAAAC/lol-blowfish.gif">
-					<div class="r-stack">
-						<div data-id="box1" style="background: cyan; width: 300px; height: 300px;"></div>
-						<div data-id="box2" style="background: magenta; width: 200px; height: 200px;"></div>
-					</div>
-					<h2 style="margin-top: 20px;">Faktor Luar Negeri</h2>
-				</section>
-				<section data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)" data-background-image="https://media1.tenor.com/m/ezrhrT61gJUAAAAC/lol-blowfish.gif">
-					<div class="r-hstack justify-center">
-						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 500px; height: 100px; margin: 10px;">Perang Russo-Jepang</div>
-						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 500px; height: 200px; margin: 10px;">Gerakan Nasionalisme di berbagai wilayah di penjuru Asia</div>
-					</div>
-				</section>
+				<section data-background-image="https://www.researchgate.net/publication/381290731/figure/fig1/AS:11431281250648852@1717957759280/Gambar-2-Peta-Kota-Surabaya-berdasarkan-Permukiman-Penduduk-Kulit-Putih-Eropa-blok.png" />
+				<section data-background-image={'./img/yes.jpeg'} />
+				<section data-background-image={'./img/ahihihi.png'} />
+				<section data-background-image="https://www.rumah123.com/seo-cms/assets/large_Balai_Kota_Surabaya_411323e29e/large_Balai_Kota_Surabaya_411323e29e.jpg" />
+				<section data-background-image="https://i.kym-cdn.com/photos/images/original/002/889/037/f2e.jpeg" />
 			</section>
-			<section data-background-image="https://ssl1.sephorastatic.cn/prodpicture/static.gif">
-				<p style="font-size: 2rem;">To all SMANTAS colleague, think about it...</p>
-				<p style="font-size: 2rem;">From the late Mohammad Hatta to a disgusting perverted misogynistic cunt</p>
-				<p style="font-size: 2rem">What could be possibly wrong with this nation?</p>
-				<p style="font-size: larger;">BRADFORD'S ALUMNI YET HIS FUCKING GPA 2.3</p>
-				<q style="font-size: larger;">Representative of the Future Brilliant Youth</q>,
-				<q style="font-size: larger;">Prodigal son worthy of leading 300 million souls</q>,
-				<q style="font-size: larger;">The Surakarta's Messiah</q>,
-				<h1 style="font-size: 4rem;">Alangkah baiknya jika spesies satu ini bunuh diri saja</h1>
-				<h2 style="font-size: 2rem">Apa? lapor ke kapolda jatim perihal pasal penghinaan wakil presiden? GO FOR IT. I couldn't care less. I'd rather rot in cell than having seeing his face every day</h2>
+
+			<section>
+				<section data-background-image="https://media.tenor.com/Y0jZtli2uUcAAAAM/toronto-blue-jays-ace.gif">
+					<h1 style="font-size: 4rem; color: black">Pendidikan</h1>
+				</section>
+				<section data-background-image="https://roodebrugsoerabaia.com/wp-content/uploads/2016/07/hbs-asli.jpg" />
+				<section data-background-image="https://awsimages.detik.net.id/community/media/visual/2021/05/27/sman-5-surabaya-1_43.jpeg?w=1200" />
+				<section data-background-image="https://itb.ac.id/files/97/20131103/1383484291.jpg" />
+				<section data-background-image="https://assets.kompasiana.com/items/album/2021/02/17/marie-thomas-05-602d1293d541df0eb460b995.jpg?t=o&v=300" />
+				<section data-background-image="https://cms.disway.id/uploads/2531533c7fb577f5b5be1217e868b184.jpg" />
+			</section>
+
+			<section>
+				<section data-background-image="https://media1.tenor.com/m/R9VGT3L_jKAAAAAC/pink-guy-grin.gif">
+					<h1 style="font-size: 4rem; color: black">Sosial</h1>
+				</section>
+				<section data-background-image="https://media1.tenor.com/m/sU1fhmL3MCoAAAAC/learn-korean-korean.gif">
+					<p style="font-size: 4rem; color: black">Literasi</p>
+				</section>
+				<section data-background-iframe="https://id.wikipedia.org/wiki/Daftar_kata_serapan_dari_bahasa_Belanda_dalam_bahasa_Indonesia#S" data-background-interactive />
+				<section data-background-image="https://folklife.si.edu/Media/Default/Talk%20Story/rijsttafel.jpg">
+					<h2 style="font-size: 4rem; color: black">Rijsttafel</h2>
+				</section>
+				<section data-background-image="https://media.suara.com/pictures/970x544/2022/11/12/44981-ilustrasi-sajian-spikoe-kuno-kue-lapis-khas-surabaya-envato-elements.jpg">
+					<h3 style="font-size: 4rem; color: whitesmoke">Kue Spikoe</h3>
+				</section>
+				<section data-background-iframe="https://voi.id/memori/7217/asal-usul-kata-pribumi-dan-mentalitas-inlander" data-background-interactive />
+			</section>
+
+			<section>
+				<section>
+					<h1 style="font-size: 4rem;">Politik</h1>
+				</section>
+				<section data-background-image="https://www.kenimedia.id/wp-content/uploads/2022/09/Kitab-Undang-Undang-Hukum-Pidana-3-scaled.jpg" />
+				<section data-background-image="https://images-tm.tempo.co/all/2024/07/25/866203/866203_720.jpg"></section>
 			</section>
 		</div>
 	</div>
