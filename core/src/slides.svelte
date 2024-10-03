96268d916576abbada1c71f184a5238ce8f7d590
archie <mydeadass1568@gmail.com>
2024-10-03 16:40:18 +0000

    fin

--- core/src/slides.svelte
+++ core/src/slides.svelte
@@ -3,6 +3,8 @@
 	import DeJavascheBank from "../src/img/ahaha.jpg";
 	import Citroen from "../src/img/yes.jpeg";
 	import Grave from "../src/img/ahihihi.png";
+	import antiJapaneseImperialism from "./img/2012_Anti-Japan_demonstrations2.jpg";
+	import filinoIndepenceDayCelebration from "./img/2020-06_ph-independence_01.jpg";
 </script>
 
 <Presentation>
@@ -72,11 +74,107 @@
 				<section data-background-image="https://pbs.twimg.com/media/GVtmr09a8AIK4JI?format=jpg&name=large">
 					<h1 style="font-size: 4rem;">Pergerakan Nasional Indonesia</h1>
 				</section>
-
-				<section>
-					<h1>Kebangkitan Nasionalisme Asia</h1>
+				<section data-background-image="https://media1.tenor.com/m/1ndoBtyucUsAAAAd/asian.gif">
+					<h1 style="font-size: 4rem;">Kebangkitan Nasionalisme Asia</h1>
 				</section>
+				<section data-background-image="https://i.pinimg.com/originals/93/a0/d6/93a0d65f26e1f93a1d3a98276062ded1.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+					<h2 style="font-size: 4rem; color: burlywood">Faktor dari Dalam Negeri</h2>
+					<p style="font-size: 3rem">Faktor dari dalam negeri yang mempengaruhi tumbuhnya nasionalisme antara lain sebagai berikut:</p>
+					<div class="r-hstack justify-center">
+						<div data-id="box1" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box2" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box3" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+					</div>
+				</section>
+				<section data-background-image="https://i.pinimg.com/originals/93/a0/d6/93a0d65f26e1f93a1d3a98276062ded1.gif" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+					<div class="r-hstack justify-center">
+						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 250px; height: 100px; margin: 10px; color:black">Kesamaan nasib</div>
+						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 250px; height: 130px; margin: 10px; color:black">kemunculan kaum terpelajar</div>
+						<div data-id="box3" data-auto-animate-delay="0.2" style="background: yellow; width: 350px; height: 180px; margin: 10px; color:black">kemajuan dalam bidang politik, sosial & ekonomi, & kebudayaan</div>
+					</div>
+					<h2 style="margin-top: 20px; font-size: 4rem; color: burlywood">Faktor dari dalam negeri</h2>
+				</section>
+				<section data-background-image="https://i.pinimg.com/originals/cf/7b/ee/cf7beecdebb61300a4dfec7008860e5f.gif">
+					<h1 style="font-size: 4rem; color:white">Faktor dari Luar Negeri</h1>
+				</section>
+				<section data-background-image="https://assets.editorial.aetnd.com/uploads/2018/03/gettyimages-514890738.jpg">
+					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 1rem; text-align: left;">
+						<h2 style="font-size: 2rem;">Kemenangan 日本 atas Россия dalam perang thn 1905</h2>
+						<p>Kemenangan Jepang dipandang sebagai simbol harapan bagi banyak bangsa terjajah di Asia. Kemenangan ini menunjukkan bahwa imperialisme Eropa dapat dilawan dan bahwa bangsa non-Barat dapat memodernisasi, melakukan industrialisasi, dan menang melawan kekuatan besar Barat. Perang ini memicu gerakan nasionalis di seluruh Asia, yang menginspirasi orang-orang yang berada di bawah kekuasaan kolonial untuk menantang penjajah mereka</p>
+					</div>
+				</section>
+				<section data-background-image={antiJapaneseImperialism}>
+					<h1 style="font-size: 4rem; color:burlywood">Gerakan Nasionalisme di 中国</h1>
+				</section>
+				<section data-background-image="https://newbloommag.net/wp-content/uploads/2020/10/97009215_10156739946006065_8208255558667468800_o-1.jpg">
+					<div style="position: left; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 1rem; text-align: left;">
+						<h2 style="font-size: 2rem;">三民主義 / 3 prinsip kerakyatan</h2>
+						<p>
+							dalam 3 prinsip kerakyatan, Sun berpendapat bahwa Wangsa Qing yang memerintah 中国 adalah monarki yang kolot dan anti-nasionalisme, oleh karena itu
+							ia bercita-cita membentuk pemerintahan yang terpusat yang demokratis, juga mengangkat harkat dan martabat bangsa Cina setara dengan masyarakat di negeri barat
+						</p>
+					</div>
+				</section>
+				<section data-background-image="https://www.mkgandhi.org/images/gandhi21.jpg">
+					<h1 style="font-size: 4rem; color:black">Gerakan Nasionalisme di भारत</h1>
+				</section>
+				<section data-background-image="https://www.mkgandhi.org/images/gandhi21.jpg">
+					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2 style="font-size: 2rem;">The Salt March</h2>
+						<p>
+							Lukisan yang kalian lihat sekarang adalah ketika bapa pendiri भारत, Mohandas Gandhi melakukan protes damai yaitu berjalan dari kota ke kota selama 24 hari untuk
+							menentang pemerintah kolonial Inggris. Dinamakan The salt march, karena Gandhi tidak setuju terhadap monopoli garam yang diisukan oleh Inggris 
+						</p>
+					</div>
+				</section>
+				<section data-background-image="https://www.mkgandhi.org/images/gandhi21.jpg" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+					<h2 style="font-size: 4rem; color: black">Satyagraha</h2>
+					<p style="font-size: 2rem; color: black">Selama Gandhi memimpin gerakan nasionalisme di भारत, ia mencetuskan 4 asas yang dimana nantinya menginspirasi masyarkat भारत untuk tetap berjuang melawan koloniasme Inggris</p>
+					<div class="r-hstack justify-center">
+						<div data-id="box1" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box2" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box3" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box4" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+						<div data-id="box5" style="background: #999; width: 50px; height: 50px; margin: 10px; border-radius: 5px;"></div>
+					</div>
+				</section>
+				<section data-background-image="https://livewirecalgary.com/wp-content/uploads/2022/08/20220815-India-Independence-City-Hall-AT-0308.jpg" data-auto-animate data-auto-animate-easing="cubic-bezier(0.770, 0.000, 0.175, 1.000)">
+					<div class="r-hstack justify-center">
+						<div data-id="box1" data-auto-animate-delay="0" style="background: cyan; width: 150px; height: 100px; margin: 10px; color:black">Satya</div>
+						<div data-id="box2" data-auto-animate-delay="0.1" style="background: magenta; width: 150px; height: 100px; margin: 10px; color:black">Ahimsa</div>
+						<div data-id="box3" data-auto-animate-delay="0.2" style="background: yellow; width: 160px; height: 100px; margin: 10px; color:black">Tapasya</div>
+						<div data-id="box4" data-auto-animate-delay="0.2" style="background: brown; width: 150px; height: 100px; margin: 10px; color:black">Hartal</div>
+						<div data-id="box5" data-auto-animate-delay="0.2" style="background: white; width: 190px; height: 100px; margin: 10px; color:black">Swadeshi</div>
+
+					</div>
+					<h2 style="margin-top: 20px; font-size: 4rem; color: black">Satyagraha</h2>
+				</section>
+				<section data-background-image="https://www.insightturkey.com/images/news/2018/01/29/ataturk-dua.jpg">
+					<h1 style="font-size: 4rem; color:burlywood">Gerakan Nasionalisme di Türkiye</h1>
+				</section>
+				<section data-background-image="https://c4.wallpaperflare.com/wallpaper/467/402/484/mustafa-kemal-ataturk-wallpaper-preview.jpg">
+					<div style="position: absolute; width: 40%; right: 0; box-shadow: 0 1px 4px rgba(0,0,0,0.5), 0 5px 25px rgba(0,0,0,0.2); background-color: rgba(0, 0, 0, 0.9); color: #fff; padding: 20px; font-size: 20px; text-align: left;">
+						<h2 style="font-size: 2rem;">Padişah'a yıkılsın!</h2>
+						<p>
+							Salah satu alasan mengapa Türkiye menjadi negara yang kita kenal sekarang adalah karena kontribusi dari Mustafa Kemal Atatürk. Ia adalah
+							Bapa pendiri Türkiye yang berhasil meruntuhkan Devlet-i ʿAlīye-i ʿOsmānīye (Kesultanan Ottoman) yang dimana pada saat itu, Ottoman mengalami
+							kekalahan yang sangat parah setelah PD 1 (Sick man of Europe)
+						</p>
+					</div>
+				</section>
+				<section data-background-image={filinoIndepenceDayCelebration}>
+					<h1 style="font-size: 4rem; color:black">Gerakan Nasionalisme di Filipina</h1>
+				</section>
+				<section></section>
 			</section>
+		</div>
+
+		<div class="reveal">
+			<div class="slides">
+				<section>
+					<section data-background-image="https://i.pinimg.com/originals/0c/ea/4d/0cea4dffc276d22ce1e3ac85092cfa9c.gif" />
+				</section>
+			</div>
 		</div>
 	</div>
 </Presentation>
